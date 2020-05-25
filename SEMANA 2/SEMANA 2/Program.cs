02c2965cc32be741d0ab1263b9400d07969b0b9c
Cloud2Go - Vitor Wurdel <vitor.wurdel@cloud2go.com.br>
2020-05-25 08:45:29 -0300

    Ajustes

---
+++ SEMANA 2/SEMANA 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEMANA_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
